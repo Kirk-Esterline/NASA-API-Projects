bd5b63da000029f7a788ceed77779d1db7f79a6c
Kirk Esterline <kirk.esterline@gmail.com>
2024-10-24 16:53:00 -0400

    started setting up ejs as a view engine. The routes seem to be causeing problems so that will be a tomorrow problem

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+const logger = require('morgan')
+const mainRoutes = require('./routes/main')
+const cameraRoutes = require('./routes/cameras')
+
+// Load Config
+dotenv.config({ path: './config/.env' })
+
+app.set('view engine', 'ejs')
+app.use(express.static('/public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(logger('dev'))
+
+
+
+const PORT = process.env.PORT || 5000
+
+app.use('/', mainRoutes)
+
+app.listen(PORT, () =>{
+    console.log(`Your port is running on Port ${PORT}: you had better go catch it.`)
+})

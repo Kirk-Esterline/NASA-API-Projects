0b9d76af1be8cebed8afc9cbcc94d42287c98744
Kirk Esterline <kirk.esterline@gmail.com>
2024-10-30 13:33:43 -0400

    Removed the code that was added for Netlify hosting.

--- api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const serverless = require("serverless-http")
-const router = express.Router();
-const api = express();
-
-
-api.use('../server/', router)
-
-export const handler = serverless(api)
